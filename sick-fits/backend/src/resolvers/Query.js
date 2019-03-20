f0e89daca2f758e939d444c5b002ad35ca75f536
Csaba Kiss <kiss.csaba.oct@gmail.com>
2019-03-20 20:16:13 +0100

    Added item model with schema

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,10 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+    async items (parent, args, context, info) {
+        const items = await context.db.query.items();
+        return items;
+    }
+};
 
 module.exports = Query;
