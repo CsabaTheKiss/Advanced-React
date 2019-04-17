71d27ca11ee58943cfa6247b45b49da77abca9e4
Csaba Kiss <kiss.csaba.oct@gmail.com>
2019-04-17 19:25:03 +0200

    Animating cart item indicator

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -10,6 +10,7 @@ import {
 import NavStyles from './styles/NavStyles';
 import User from './User';
 import Signout from './Signout';
+import CartCount from './CartCount';
 
 import {
   TOGGLE_CART_MUTATION
@@ -39,6 +40,7 @@ const Nav = () => (
                   {(toggleCart) => 
                     <button onClick={toggleCart}>
                       My Cart
+                      <CartCount count={me.cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0)} />
                     </button>
                   }
                 </Mutation>
